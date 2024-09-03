3290559cb5755fa522a65a58e2c4f917804046f1
gandyrosales <62707234+gandyrosales@users.noreply.github.com>
2024-09-03 21:57:40 +0900

    Update CCSEddyDetectorRun.m

--- CCSEddyDetectorRun.m
+++ CCSEddyDetectorRun.m
@@ -4,19 +4,20 @@ close all
 clc
 
 
-pathin =  'your/output/data';
+pathin =  'your/output/data/folder';
 basein='name_output';
+% use any year needed, e.g. Y7 to Y8
 sy=7; %year initial
 ey=8; % year end
 sm=1; % month initial
 em=12; % month end
 
-
+% layers criteria
 depi=-0; % for surface
 depe=-50; % for surface
-Isoi= 25.5;
-Isoe = 26.5;
-crow= -2e-11;
+Isoi= 25.5; %  for subsurface
+Isoe = 26.5; % for subsurface
+crow= -2e-11; % criteria for OW
 crar=[pi*10000^2 pi*200000^2]; 
 plotflag=10;
 OW_smooth = 10;
